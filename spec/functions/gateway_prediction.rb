87d3dadf2d0a056766c6571f8d00eabb904d07cb
Alexander Ursu <alexander.ursu@gmail.com>
2021-10-06 12:24:10 +0200

    Added function gateway_prediction

--- spec/functions/gateway_prediction.rb
+++ spec/functions/gateway_prediction.rb
@@ -9,5 +9,17 @@ describe 'networksetup::gateway_prediction' do
     it {
       is_expected.to run.with_params('192.168.10.30/28').and_return('192.168.10.17')
     }
+
+    it {
+      is_expected.to run.with_params('192.168.10.30', '255.255.255.0').and_return('192.168.10.1')
+    }
+
+    it {
+      is_expected.to run.with_params('192.168.10.30', 28).and_return('192.168.10.17')
+    }
+
+    it {
+      is_expected.to run.with_params('192.168.10.30', nil).and_return('192.168.10.1')
+    }
   end
 end
